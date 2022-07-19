23446578c9c8c325a42f9a81916d8397eb073c42
Shreyansh.jain47 <shreyansh.jain047@gmail.com>
2022-07-19 13:07:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubActions_Upload_to_S3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
